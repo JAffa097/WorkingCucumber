03b307beab65b2f62acf332e28ee62ddc9fe91a6
JAffa097 <jpgpgma666@gmail.com>
2023-04-11 00:16:33 +0530

    initial working cucumber project

---
+++ Cucumber/src/main/java/stepDefinitions/Runner.java
@@ -0,0 +1,10 @@
+package stepDefinitions;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(tags= "@tag2",features="src\\test\\resources\\test.feature")
+public class Runner extends AbstractTestNGCucumberTests{
+	
+
+}
